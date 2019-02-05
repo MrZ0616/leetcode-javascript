70a204e47d2d882bd63c0fcfba0501e2734f5ebf
mrz0616 <2864644417@qq.com>
2019-02-05 09:45:03 +0800

    724

---
+++ 724.寻找数组的中心索引.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let pivotIndex = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let lSum = 0
+    let rSum = 0
+
+    for (let j = 0; j < i; j++) {
+      lSum += nums[j]
+    }
+
+    for (let j = i + 1; j < nums.length; j++) {
+      rSum += nums[j]
+    }
+
+    if (lSum === rSum) {
+      return i
+    }
+  }
+
+  return -1
+};
+
+console.log(pivotIndex([1, 7, 3, 6, 5, 6]))
+console.log(pivotIndex([1, 2, 3]))
