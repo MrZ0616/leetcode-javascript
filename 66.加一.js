1ba41630c60e209785f9be9adf2cd321e8a967d0
mrz0616 <2864644417@qq.com>
2019-02-07 09:02:02 +0800

    66

---
+++ 66.加一.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+let plusOne = function(digits) {
+  let carry = 0
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let num = i === digits.length - 1 ? digits[i] + carry + 1 : digits[i] + carry
+    digits[i] = num % 10
+    carry = Math.floor(num / 10)
+  }
+
+  if (carry > 0) {
+    digits.unshift(carry)
+  }
+
+  return digits
+};
+
+console.log(plusOne([4,3,2,1]))
+console.log(plusOne([4,3,2,9]))
+console.log(plusOne([9,9,9,9]))
