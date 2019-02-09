e59ebfe9539e650391e0c8ac7336acb40357ab52
mrz0616 <2864644417@qq.com>
2019-02-09 09:06:18 +0800

    118

---
+++ 118.杨辉三角.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+let generate = function (numRows) {
+  if (numRows <= 0) return []
+
+  let arr = [[1]]
+  let row = 0
+  let col = 0
+  for (let i = 1; i < numRows; i++) {
+    let arrRow = []
+    for (let j = 0; j < i + 1; j++) {
+      let upLeft = arr[i - 1][j - 1] === undefined ? 0 : arr[i - 1][j - 1]
+      let upRight = arr[i - 1][j] === undefined ? 0 : arr[i - 1][j]
+      arrRow.push(upLeft + upRight)
+    }
+
+    arr.push(arrRow)
+  }
+
+  return arr
+};
+
+console.log(generate(5))
