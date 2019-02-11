cf78841c7319a44b00ad1c9eb21a8a9c49ed4fae
mrz0616 <2864644417@qq.com>
2019-02-11 08:12:56 +0800

    344

---
+++ 344.反转字符串.js
@@ -0,0 +1,20 @@
+/**
+ * @param {String[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+let reverseString = function(s) {
+  if (s === undefined || s.length === 0) return
+
+  let left = 0
+  let right = s.length - 1
+
+  while (left < right) {
+    let ch = s[left]
+    s[left] = s[right]
+    s[right] = ch
+    left++
+    right--
+  }
+};
+
+reverseString(['h', 'e', 'l', 'l', 'o'])
