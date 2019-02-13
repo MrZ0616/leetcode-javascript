cd09da9f17697b6e7d11d2fc63da1195a6651801
mrz0616 <2864644417@qq.com>
2019-02-13 10:14:18 +0800

    561

---
+++ 561.数组拆分-I.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let arrayPairSum = function (nums) {
+  nums.sort(((a, b) => a - b))
+
+  let sum = 0
+  for (let i = 0; i < nums.length; i = i + 2) {
+    sum += nums[i]
+  }
+
+  return sum
+};
+
+console.log(arrayPairSum([1, 4, 3, 2]))
+console.log(arrayPairSum([6214, -2290, 2833, -7908]))
