ec84579267f65b7b86ac6863d68649fb55e3b3ad
mrz0616 <2864644417@qq.com>
2019-02-17 09:30:12 +0800

    119

---
+++ 119.杨辉三角-II.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+
+  let arr = [1]
+
+  for (let i = 0; i < rowIndex; i++) {
+    arr.unshift(0)
+
+    for (let j = 0; j < arr.length - 1; j++) {
+      arr[j] = arr[j] + arr[j + 1]
+    }
+  }
+
+  return arr
+};
+
+console.log(getRow(5))
