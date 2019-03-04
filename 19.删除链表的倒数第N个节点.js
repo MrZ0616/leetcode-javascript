aed675e349fe3611c8927b427ee634e015b7ab5c
mrz0616 <2864644417@qq.com>
2019-03-04 09:36:33 +0800

    19

---
+++ 19.删除链表的倒数第N个节点.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+const ListNode = require('./public/listnode')
+
+const removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode()
+  dummy.next = head
+
+  // 双指针
+  let slow = dummy, fast = dummy
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next
+  }
+
+  while (fast.next != null) {
+    slow = slow.next
+    fast = fast.next
+  }
+
+  slow.next = slow.next.next
+  return dummy.next
+};
