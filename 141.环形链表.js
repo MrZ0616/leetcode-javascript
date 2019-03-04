7c0ab14d9348595c57e3829658ff9366b93a6c81
mrz0616 <2864644417@qq.com>
2019-03-04 11:23:58 +0800

    141

---
+++ 141.环形链表.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = function (head) {
+  while (head && head.next) {
+    if (head.visted) {
+      return true
+    } else {
+      head.visted = true
+      head = head.next
+    }
+  }
+
+  return false
+};
