7b340141b81989ff0b21b07f31e9ea8330b864ad
mrz0616 <2864644417@qq.com>
2019-03-04 10:20:40 +0800

    21

---
+++ 21.合并两个有序链表.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const ListNode = require('./public/listnode')
+
+const mergeTwoLists = function (l1, l2) {
+  let dummy = new ListNode()
+  let prev = dummy
+
+  while (l1 != null && l2 != null) {
+    if (l1.val <= l2.val) {
+      prev.next = l1
+      l1 = l1.next
+    } else {
+      prev.next = l2
+      l2 = l2.next
+    }
+    prev = prev.next
+  }
+
+  prev.next = l1 ? l1 : l2
+
+  return dummy.next
+};
