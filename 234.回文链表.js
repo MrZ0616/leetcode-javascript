001cd44351841ce95a4bb196abb6da8a984cc508
mrz0616 <2864644417@qq.com>
2019-03-04 10:52:32 +0800

    234

---
+++ 234.回文链表.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const isPalindrome = function (head) {
+  let vals = []
+
+  while (head) {
+    vals.push(head.val)
+    head = head.next
+  }
+
+  while (vals.length > 1) {
+    if (vals.shift() !== vals.pop()) return false
+  }
+
+  return true
+};
