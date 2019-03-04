f791c780eec697002d88e09c3e24e8b14b393601
mrz0616 <2864644417@qq.com>
2019-03-04 09:55:38 +0800

    206

---
+++ 206.反转链表.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+  let prev = null
+  let curr = head
+
+  while (curr != null) {
+    let next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+
+  return prev
+};
