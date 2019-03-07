2db8aae816a8e07f942959d68c3ce03df611f9c4
mrz0616 <2864644417@qq.com>
2019-03-08 07:52:50 +0800

    108

---
+++ 108.将有序数组转换为二叉搜索树.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+const TreeNode = require('./public/treenode')
+
+const sortedArrayToBST = function (nums) {
+  if (!nums.length) return null
+
+  let root = new TreeNode(null)
+  if (nums.length > 1) {
+    root.left = sortedArrayToBST(nums.splice(0, nums.length / 2))
+  }
+  root.val = nums[0]
+  root.right = sortedArrayToBST(nums.splice(1))
+
+  return root
+};
