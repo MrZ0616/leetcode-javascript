1e87f2e6c341b8529f51d799ad0583ed969258de
mrz0616 <2864644417@qq.com>
2019-03-08 07:59:17 +0800

    98

--- 98.验证二叉搜索树.js
+++ 98.验证二叉搜索树.js
@@ -9,8 +9,19 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+const TreeNode = require("./public/treenode")
+
 const isValidBST = function (root, min = -Infinity, max = Infinity) {
   if (!root) return true
   if (root.val <= min || root.val >= max) return false
   return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max)
 };
+
+root = new TreeNode(5)
+root.left = new TreeNode(1)
+root.right = new TreeNode(7)
+root.right.left = new TreeNode(6)
+root.right.right = new TreeNode(8)
+
+console.log(isValidBST(root))
+
