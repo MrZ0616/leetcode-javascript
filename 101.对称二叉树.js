dcfdc0ce0b6c977859521143f80e12b1305a4a36
mrz0616 <2864644417@qq.com>
2019-03-07 19:51:49 +0800

    101

---
+++ 101.对称二叉树.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isSymmetric = function (root) {
+  if (root === null) return true
+
+  function dfs(left, right) {
+    if (left === null && right === null) return true
+    if (!(left !== null && right !== null)) return false
+    if (left.val !== right.val) return false
+
+    return dfs(left.left, right.right) && dfs(left.right, right.left)
+  }
+
+  return dfs(root.left, root.right)
+};
