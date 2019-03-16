bb859e29fb9fe6b924554069ceae4a370025e5aa
mrz0616 <2864644417@qq.com>
2019-03-16 10:42:01 +0800

    189

--- 189.旋转数组.js
+++ 189.旋转数组.js
@@ -3,8 +3,9 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// 暴力
 const rotate = function (nums, k) {
-  if (nums === undefined || nums.length === 0) return
+  if (!nums || nums.length === 0) return
 
   for (let j = 0; j < k; j++) {
     // 向右旋转一位
@@ -15,3 +16,30 @@ const rotate = function (nums, k) {
     nums[0] = last
   }
 }
+
+// 环状替换
+const rotate1 = function (nums, k) {
+  if (!nums || nums.length === 0) return
+
+  k = k % nums.length
+  let count = 0
+
+  for (let starIndex = 0; count < nums.length; starIndex++) {
+    let currIndex = starIndex
+    let prev = nums[starIndex]
+
+    do {
+      let nextIndex = (currIndex + k) % nums.length
+      let temp = nums[nextIndex]
+      nums[nextIndex] = prev
+      prev = temp
+      currIndex = nextIndex
+      count++
+    } while (starIndex !== currIndex)
+  }
+
+  return nums
+}
+
+console.log(rotate1([1, 2, 3, 4, 5, 6], 2))
+console.log(rotate1([1, 2, 3, 4, 5, 6, 7], 2))
