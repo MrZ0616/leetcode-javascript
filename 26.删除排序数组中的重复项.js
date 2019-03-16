56db5c6a03363a2906bef4d3876cd8cf9872534c
mrz0616 <2864644417@qq.com>
2019-03-16 09:54:41 +0800

    26

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -1,23 +1,21 @@
 /**
  * @param {number[]} nums
  * @return {number}
+ * 双指针
  */
 const removeDuplicates = function (nums) {
-  if (nums === undefined || nums.length === 0) return 0
+  if (!nums || nums.length === 0) return 0
 
-  let prev = nums[0]
-  let j = 1 // 插入指针
+  let j = 0 // 插入指针
   for (let i = 1; i < nums.length; i++) {
-    if (prev !== nums[i]) {
-      nums[j] = nums[i]
-      prev = nums[i]
-      j++
+    if (nums[j] !== nums[i]) {
+      nums[++j] = nums[i]
     }
   }
 
-  return j
+  return j + 1
 }
 
-// console.log(removeDuplicates([1, 1, 2]))
+console.log(removeDuplicates([1, 1, 2]))
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
 console.log(removeDuplicates([1, 2, 3]))
